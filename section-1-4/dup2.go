e1fe7ffea2b29a4c65d264c551d52a6f6d51408b
Leandro Lima <llimas@outlook.com>
2021-09-24 22:50:41 -0300

    solution of dup2 section 1-4

--- section-1-4/dup2.go
+++ section-1-4/dup2.go
@@ -8,10 +8,10 @@ import (
 
 // change dup2 to show the name of all files which have duplicated lines <- missing
 func main() {
-	counts := make(map[string]int)
+	counts := make(map[string][]map[string]int)
 	files := os.Args[1:]
 	if len(files) == 0 {
-		countLines(os.Stdin, counts)
+		countLines(os.Stdin, counts, "test")
 	} else {
 		for _, arg := range files {
 			f, err := os.Open(arg)
@@ -19,23 +19,39 @@ func main() {
 				fmt.Fprintf(os.Stderr, "dup2 : %v\n", err)
 				continue
 			}
-			countLines(f, counts)
+			countLines(f, counts, arg)
 			f.Close()
 		}
 	}
 
-	for line, n := range counts {
-		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
+	for palavra, arrMaps := range counts {
+		fmt.Println(palavra)
+		for _, valor := range arrMaps {
+			fmt.Println(valor)
 		}
 	}
 
 }
 
-func countLines(f *os.File, counts map[string]int) {
-	fmt.Println(f)
+func countLines(f *os.File, counts map[string][]map[string]int, arg string) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
-		counts[input.Text()]++
+		arquivo := make(map[string]int)
+		arquivo[arg]++
+		if _, ok := counts[input.Text()]; ok {
+			added := false
+			for _, val := range counts[input.Text()] {
+				if _, ok := val[arg]; ok {
+					val[arg]++
+					added = true
+				}
+			}
+			if !added {
+				counts[input.Text()] = append(counts[input.Text()], arquivo)
+			}
+		} else {
+			counts[input.Text()] = append(counts[input.Text()], arquivo)
+		}
+
 	}
 }
